926451f90705deea7a840a18a650d2a3ddda84bc
bradshac <bradshac@rose-hulman.edu>
2018-02-01 10:52:14 -0500

    Put my name in member 4

--- sandbox/Bill/digital_inputs/m2_button_events_with_sound.py
+++ sandbox/Bill/digital_inputs/m2_button_events_with_sound.py
@@ -53,7 +53,7 @@ def main():
     btn.on_up = handle_up_button
     btn.on_down = handle_down_button
     btn.on_left = handle_left_button
-    btn.on_right = handle_down_button
+    btn.on_right = handle_right_button
 
     # TODO: 5. Note #4 is lower (this is TO DO #5 which you should do after #4).
     # Add a lambda callback for on_backspace.  The syntax of lambda is:
