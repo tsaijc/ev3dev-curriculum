bc0b7c4e6e9f4875d06e9377d3183c41c76bc475
fangz2 <fangz2@rose-hulman.edu>
2018-02-01 11:10:52 -0500

    digital m3

--- sandbox/Bill/digital_inputs/m3_ir_events_with_the_screen.py
+++ sandbox/Bill/digital_inputs/m3_ir_events_with_the_screen.py
@@ -66,6 +66,13 @@ def main():
     # Object that is storing references to images that can be passed into callbacks.
     dc = DataContainer()
     rc1 = ev3.RemoteControl(channel=1)
+    rc2 = ev3.RemoteControl(channel=2)
+    rc3 = ev3.RemoteControl(channel=3)
+    rc4 = ev3.RemoteControl(channel=4)
+    assert rc1.connected
+    assert rc2.connected
+    assert rc3.connected
+    assert rc4.connected
     display_image(dc.lcd_screen, dc.eyes)  # Display an image on the EV3 screen
     ev3.Sound.speak("I R events with the Screen").wait()
 
@@ -74,7 +81,7 @@ def main():
     #   .on_red_down  to call handle_red_down_1  (that exist already) with state and dc as parameters
     #   .on_blue_up   to call handle_blue_up_1   (that exist already) with state and dc as parameters
     #   .on_blue_down to call handle_blue_down_1 (that exist already) with state and dc as parameters
-    rc1.on_red_up =  lambda state: handle_red_up_1(state, dc)
+    rc1.on_red_up = lambda state: handle_red_up_1(state, dc)
     rc1.on_red_down = lambda state: handle_red_down_1(state, dc)
     rc1.on_blue_up = lambda state: handle_blue_up_1(state, dc)
     rc1.on_blue_down = lambda state: handle_blue_down_1(state, dc)
@@ -83,7 +90,9 @@ def main():
     #   Channel 2's .on_red_up should call handle_red_up_2 (that exist already) with state and dc as parameters
     #   Channel 3's .on_red_up should call handle_red_up_3 (that exist already) with state and dc as parameters
     #   Channel 4's .on_red_up should call handle_red_up_4 (that exist already) with state and dc as parameters
-
+    rc2.on_red_up = lambda state: handle_red_up_2(state, dc)
+    rc3.on_red_up = lambda state: handle_red_up_3(state, dc)
+    rc4.on_red_up = lambda state: handle_red_up_4(state, dc)
     # Buttons on EV3
     btn = ev3.Button()
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
@@ -91,10 +100,12 @@ def main():
     while dc.running:
         # TODO: 4. Call the .process() method on your channel 1 RemoteControl object, then review and run your code.
         #   Review the handle functions below to see how they draw to the screen.  They are already finished.
-
+        rc1.process()
         # TODO: 6. Call the .process() method on your channel 2 - 4 RemoteControl objects and demo your code.
         #   Review the handle functions below to see how they draw to the screen.  They are already finished.
-
+        rc2.process()
+        rc3.process()
+        rc4.process()
         # TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
         #
         # Observations you should make, IR buttons work exactly like buttons on the EV3.
