50c5c7747f787278395e060eba6349ad19c525e5
tsaijc <tsaijc@rose-hulman.edu>
2018-02-06 18:40:42 -0500

    added doc strings

--- sandbox/tsaijc/mqtt/m4_pc_petals_on_a_rose.py
+++ sandbox/tsaijc/mqtt/m4_pc_petals_on_a_rose.py
@@ -89,7 +89,7 @@ def guess(mqtt_client, number_to_guess_entry):
 
 def set_num_dice(mqtt_client, num_dice_entry):
     """ Calls a method on EV3 called 'set_number_of_dice' passing in an int from the num_dice_entry. """
-    # TODO: 6. Write the line of code necessary to implement this method based on the doc string's description.
+    # DONE: 6. Write the line of code necessary to implement this method based on the doc string's description.
     mqtt_client.send_message('set_number_of_dice', [int(num_dice_entry.get())])
 
 
