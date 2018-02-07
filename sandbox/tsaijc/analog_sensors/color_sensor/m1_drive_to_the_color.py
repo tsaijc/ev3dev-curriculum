7d09206550bb4c2b699c9d33070f5e5993edb256
tsaijc <tsaijc@rose-hulman.edu>
2018-02-07 11:12:58 -0500

    added doc strings

--- sandbox/tsaijc/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/tsaijc/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -95,14 +95,9 @@ def drive_to_color(button_state, robot, color_to_seek):
         #   assert self.color_sensor
         # Then here you can use a command like robot.color_sensor.color to check the value
         robot.loop_forever()
-        if robot.color_sensor.color == ev3.ColorSensor.COLOR_RED:
+        while robot.color_sensor.color == color_to_seek:
             robot.stop()
-        if robot.color_sensor.color == ev3.ColorSensor.COLOR_BLUE:
-            robot.stop()
-        if robot.color_sensor.color == ev3.ColorSensor.COLOR_BLACK:
-            robot.stop()
-        if robot.color_sensor.color == ev3.ColorSensor.COLOR_WHITE:
-            robot.stop()
+            break
 
 
         # TODO: 4. Call over a TA or instructor to sign your team's checkoff sheet.
