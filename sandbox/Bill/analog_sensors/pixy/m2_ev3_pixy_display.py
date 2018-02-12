a11634054b6d6cff952de191c8a987a0a68a952c
fangz2 <fangz2@rose-hulman.edu>
2018-02-11 19:51:21 -0500

    pixy m2

--- sandbox/Bill/analog_sensors/pixy/m2_ev3_pixy_display.py
+++ sandbox/Bill/analog_sensors/pixy/m2_ev3_pixy_display.py
@@ -3,8 +3,8 @@
 The goal of this module is to practice using the Pixy and MQTT at the same time.  This module will send data from the
 EV3 to the PC.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.  February 2017.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and zhengshanfang.  February 2017.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -21,22 +21,28 @@ def main():
     ev3.Sound.speak("Pixy display").wait()
     print("Press the touch sensor to exit this program.")
 
-    # TODO: 2. Create an MqttClient (no delegate needed since EV3 will only send data, so an empty constructor is fine)
+    # done: 2. Create an MqttClient (no delegate needed since EV3 will only send data, so an empty constructor is fine)
     # Then connect to the pc using the connect_to_pc method.
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_pc()
 
     robot = robo.Snatch3r()
     robot.pixy.mode = "SIG1"
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 3. Read the Pixy values for x, y, width, and height
+        # done: 3. Read the Pixy values for x, y, width, and height
         # Print the values (much like the print_pixy_readings example)
+        print("(X, Y)=({}, {}) Width={} Height={}".format(
+            robot.pixy.value(1), robot.pixy.value(2), robot.pixy.value(3),
+            robot.pixy.value(4)))
 
         # TODO: 4. Send the Pixy values to the PC by calling the on_rectangle_update method
         # If you open m2_pc_pixy_display you can see the parameters for that method [x, y, width, height]
-
-
-
+        mqtt_client.send_message("on_rectangle_update", [robot.pixy.value(1),
+                                                        robot.pixy.value(2),
+                                                        robot.pixy.value(3),
+                                                        robot.pixy.value(4)])
 
         time.sleep(0.25)
 
