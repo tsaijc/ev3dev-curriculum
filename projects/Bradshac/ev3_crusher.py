555f5b7d3ad2ff84a0206f189ad9464c8c39d89e
bradshac <bradshac@rose-hulman.edu>
2018-02-18 00:31:46 -0500

    my project

--- projects/Bradshac/ev3_crusher.py
+++ projects/Bradshac/ev3_crusher.py
@@ -3,11 +3,12 @@ import robot_controller as robo
 import ev3dev.ev3 as ev3
 import time
 
+
 class MyDelegate(object):
     def __init__(self):
         self.robot = robo.Snatch3r()
+        # self.mqtt_client = None
         # self.list_of_colors = [red, blue, green]
-
 
 
     def certain_color(self, color):
@@ -36,19 +37,11 @@ class MyDelegate(object):
 
 
 def main():
-    my_delegate = MyDelegate
+    my_delegate = MyDelegate()
     mqtt_client = com.MqttClient(my_delegate)
+    my_delegate.mqtt_client = mqtt_client
     mqtt_client.connect_to_pc()
-
-    # while not robot.touch_sensor.is_pressed:
-    #
-    #     if robot.ir_sensor.proximity < 10:
-    #         ev3.Sound.beep()
-    #         ev3.Sound.speak("We need to crush")
-    #
-    #         time.sleep(1.5)
-    #     time.sleep(0.1)
-
+    # robot = robo.Snatch3r
     my_delegate.robot.loop_forever()
 
 
