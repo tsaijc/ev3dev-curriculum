35740262b0a746f5f646ebcccf87bdd860bfb5f1
tsaijc <tsaijc@rose-hulman.edu>
2018-02-18 19:12:44 -0500

    updated project code

--- projects/tsaijc/project_code.py
+++ projects/tsaijc/project_code.py
@@ -11,17 +11,6 @@ from tkinter import ttk
 import mqtt_remote_method_calls as com
 
 
-class MyDelegate(object):
-    """ MyDelegate class that sets up display messages between EV3 and the PC."""
-    def __init__(self, label_to_display_messages_in):
-        self.display_label = label_to_display_messages_in
-
-    def button_pressed(self, button_name):
-        print("Received: " + button_name)
-        message_to_display = "{} was pressed.".format(button_name)
-        self.display_label.configure(text=message_to_display)
-
-
 class DataContainer(object):
     """ Helper class that might be useful to communicate between different callbacks."""
     def __init__(self):
@@ -46,11 +35,10 @@ def control_ev3_movements():
     main_frame = ttk.Frame(root, padding=20, relief='raised')
     main_frame.grid()
 
-    button_message = ttk.Label(main_frame, text='--')
-    button_message.grid(row=7, column=1)
+    # button_message = ttk.Label(main_frame, text='--')
+    # button_message.grid(row=7, column=1)
 
-    my_delegate = MyDelegate(button_message)
-    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
 
     left_speed_label = ttk.Label(main_frame, text="Left")
@@ -58,78 +46,114 @@ def control_ev3_movements():
     left_speed_entry = ttk.Entry(main_frame, width=8)
     left_speed_entry.insert(0, "600")
     left_speed_entry.grid(row=1, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='100', value=100)
+    # left_speed_entry.grid(row=1, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='200', value=200)
+    # left_speed_entry.grid(row=2, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='300', value=300)
+    # left_speed_entry.grid(row=3, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='400', value=400)
+    # left_speed_entry.grid(row=4, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='500', value=500)
+    # left_speed_entry.grid(row=5, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='600', value=600)
+    # left_speed_entry.grid(row=6, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='700', value=700)
+    # left_speed_entry.grid(row=7, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='800', value=800)
+    # left_speed_entry.grid(row=8, column=0)
+    # left_speed_entry = ttk.Radiobutton(main_frame, text='900', value=900)
+    # left_speed_entry.grid(row=9, column=0)
 
     right_speed_label = ttk.Label(main_frame, text="Right")
     right_speed_label.grid(row=0, column=2)
     right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='100', value=100)
+    # right_speed_entry.grid(row=1, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='200', value=200)
+    # right_speed_entry.grid(row=2, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='300', value=300)
+    # right_speed_entry.grid(row=3, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='400', value=400)
+    # right_speed_entry.grid(row=4, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='500', value=500)
+    # right_speed_entry.grid(row=5, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='600', value=600)
+    # right_speed_entry.grid(row=6, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='700', value=700)
+    # right_speed_entry.grid(row=7, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='800', value=800)
+    # right_speed_entry.grid(row=8, column=2)
+    # right_speed_entry = ttk.Radiobutton(main_frame, text='900', value=900)
+    # right_speed_entry.grid(row=9, column=2)
 
     forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid(row=2, column=1)
+    forward_button.grid(row=10, column=1)
     # forward_button and '<Up>' key
     forward_button['command'] = lambda: move_forward(mqtt_client, left_speed_entry, right_speed_entry)
     root.bind('<Up>', lambda event: move_forward(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid(row=3, column=0)
+    left_button.grid(row=11, column=0)
     # left_button and '<Left>' key
     left_button['command'] = lambda: turn_left(mqtt_client, left_speed_entry, right_speed_entry)
     root.bind('<Left>', lambda event: turn_left(mqtt_client, left_speed_entry, right_speed_entry))
 
     stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid(row=3, column=1)
+    stop_button.grid(row=11, column=1)
     # stop_button and '<space>' key (note, does not need left_speed_entry, right_speed_entry)
     stop_button['command'] = lambda: stop(mqtt_client)
     root.bind('<space>', lambda event: stop(mqtt_client))
 
     right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid(row=3, column=2)
+    right_button.grid(row=11, column=2)
     # right_button and '<Right>' key
     right_button['command'] = lambda: turn_right(mqtt_client, left_speed_entry, right_speed_entry)
     root.bind('<Right>', lambda event: turn_right(mqtt_client, left_speed_entry, right_speed_entry))
 
     back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid(row=4, column=1)
+    back_button.grid(row=12, column=1)
     # back_button and '<Down>' key
     back_button['command'] = lambda: move_back(mqtt_client, left_speed_entry, right_speed_entry)
     root.bind('<Down>', lambda event: move_back(mqtt_client, left_speed_entry, right_speed_entry))
 
     up_button = ttk.Button(main_frame, text="Up")
-    up_button.grid(row=5, column=0)
+    up_button.grid(row=13, column=0)
     up_button['command'] = lambda: send_up(mqtt_client)
     root.bind('<u>', lambda event: send_up(mqtt_client))
 
     down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid(row=6, column=0)
+    down_button.grid(row=14, column=0)
     down_button['command'] = lambda: send_down(mqtt_client)
     root.bind('<j>', lambda event: send_down(mqtt_client))
 
     tools = ttk.Label(main_frame, text="Tools To Use:")
-    tools.grid(row=7, column=1)
+    tools.grid(row=15, column=1)
 
     move_button = ttk.Button(main_frame, text="Navigate Maze")
-    move_button.grid(row=8, column=0)
+    move_button.grid(row=16, column=0)
     move_button['command'] = lambda: drive_to_color(mqtt_client)
-    root.bind('<f>', lambda event: drive_to_color(mqtt_client))
+    root.bind('<a>', lambda event: drive_to_color(mqtt_client))
 
     color_button = ttk.Button(main_frame, text="Detect Color")
-    color_button.grid(row=8, column=1)
+    color_button.grid(row=16, column=1)
     color_button['command'] = lambda: color_value(mqtt_client)
-    root.bind('<f>', lambda event: color_value(mqtt_client))
+    root.bind('<s>', lambda event: color_value(mqtt_client))
 
     beacon_find = ttk.Button(main_frame, text="Find Prize")
-    beacon_find.grid(row=8, column=2)
+    beacon_find.grid(row=16, column=2)
     beacon_find['command'] = lambda: find_prize(mqtt_client)
-    root.bind('<f>', lambda event: find_prize(mqtt_client))
+    root.bind('<d>', lambda event: find_prize(mqtt_client))
 
     # Buttons for quit and exit
     q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=5, column=2)
+    q_button.grid(row=13, column=2)
     q_button['command'] = (lambda: quit_program(mqtt_client, False))
 
     e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=6, column=2)
+    e_button.grid(row=14, column=2)
     e_button['command'] = (lambda: quit_program(mqtt_client, True))
 
     root.mainloop()
@@ -174,6 +198,26 @@ def send_up(mqtt_client):
 def send_down(mqtt_client):
     print("arm_down")
     mqtt_client.send_message("arm_down")
+
+
+def send_message(mqtt_client):
+    print("send_message")
+    mqtt_client.send_message(("send_message"))
+
+
+def drive_to_color(mqtt_client):
+    print("drive_to_color")
+    mqtt_client.send_message("drive_to_color")
+
+
+def color_value(mqtt_client):
+    print("color_value")
+    mqtt_client.send_message("color_value")
+
+
+def find_prize(mqtt_client):
+    print("find_prize")
+    mqtt_client.send_message("find_prize")
 
 
 # Quit and Exit button callbacks
