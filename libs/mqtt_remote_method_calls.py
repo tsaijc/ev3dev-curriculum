deed47d0d3ad0bfc749980fb2e6b508c23516c7f
seoh1 <seoh@rose-hulman.edu>
2018-02-18 23:48:43 -0500

    Just commit

--- libs/mqtt_remote_method_calls.py
+++ libs/mqtt_remote_method_calls.py
@@ -128,7 +128,7 @@ import json
 import collections
 import paho.mqtt.client as mqtt
 
-LEGO_NUMBER = 14  # DONE: Set your LEGO_NUMBER
+LEGO_NUMBER = 29  # DONE: Set your LEGO_NUMBER
 
 
 class MqttClient(object):
