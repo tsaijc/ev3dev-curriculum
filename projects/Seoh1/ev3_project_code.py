9d15b69053cb3e7b2de12aa5258e4d4f2823634d
seoh1 <seoh@rose-hulman.edu>
2018-02-18 23:45:41 -0500

    Just commit

---
+++ projects/Seoh1/ev3_project_code.py
@@ -0,0 +1,18 @@
+"""
+    The following are the code for CSSE 120 final project
+    Author: Hyang Seo
+    Class: CSSE 120
+    Project name/theme: Puzzle Alarm Clock
+"""
+
+import mqtt_remote_method_calls as com
+import robot_controller as robo
+
+def main():
+    robot = robo.Snatch3r()
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+    robot.loop_forever()
+
+
+main()
